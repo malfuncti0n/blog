0986f9c76d42e569f849e13b3570d94996189eb0
malfuncti0n <savvaniss@yahoo.gr>
2017-04-13 14:51:38 +0300

    create migration for user table

--- db/migrations/20170413113807_create_user_table.php
+++ db/migrations/20170413113807_create_user_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class CreateUserTable extends AbstractMigration
 {
@@ -25,8 +26,20 @@ class CreateUserTable extends AbstractMigration
      * Remember to call "create()" or "update()" and NOT "save()" when working
      * with the Table class.
      */
-    public function change()
+    public function up()
     {
+        $users=$this->table('users');
+        $users->addColumn('name', 'string', array('limit' => 50))
+              ->addColumn('email', 'string', array('limit' => 50))
+              ->addColumn('username', 'string', array('limit' => 50,'null' =>true))
+              ->addColumn('password', 'string', array('limit' => 255))
+              ->addColumn('created_at', 'timestamp', array('default' => 'CURRENT_TIMESTAMP'))
+              ->addColumn('updated_at', 'timestamp', array('null' => true))
+              ->addIndex(array('email'), array('unique' => true))
 
+              ->save();
+    }
+    public function down(){
+        $this->dropTable('users');
     }
 }
